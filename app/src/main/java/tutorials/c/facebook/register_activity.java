072eacc11c7cfb14c82aa8b08edafe41a29796b8
RAAGAVARDHINI <raagavardhini12@gmail.com>
2020-06-24 15:29:07 +0530

    Initial commit

---
+++ app/src/main/java/tutorials/c/facebook/register_activity.java
@@ -0,0 +1,14 @@
+package tutorials.c.facebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class register_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_activity);
+    }
+}
